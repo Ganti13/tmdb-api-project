80eb3a7fd8152ed037e3390531fee4234939445e
Ganti13 <mikesilvah@gmail.com>
2022-03-05 19:19:53 -0300

    updated

--- src/components/ModalSearch.jsx
+++ src/components/ModalSearch.jsx
@@ -23,10 +23,10 @@ export default ({type, handleActiveModal}) => {
 
 	return(
 		<div className="fixed z-50 bg-black bg-opacity-60 inset-0 grid place-items-center">
-				<div className="w-3/4 h-4/5 flex items-center flex-col py-4
+				<div className="w-11/12 sm:w-3/4 h-4/5 flex items-center flex-col py-4
 				bg-background-800 overflow-y-auto relative rounded-md">
 					<button
-					className="outline-none text-red-700 absolute top-2 right-2" 
+					className="outline-none text-red-700 absolute top-1 right-1 sm:top-2 sm:right-2" 
 					onClick={handleActiveModal}>
 						<AiFillCloseCircle size={36}/>
 					</button>
@@ -34,13 +34,13 @@ export default ({type, handleActiveModal}) => {
 						<input type="text"
 						value={search}
 						onChange={e => setSearch(e.target.value)}
-						className="p-2 outline-none"
+						className="p-2 outline-none w-40 sm:w-72"
 						placeholder="Pesquisar"
 						/>
 					</form>
 					<div className="w-full flex flex-wrap gap-2 justify-center p-2">
 						{items.length < 1 ? <div className="bg-background-700 w-full absolute 
-						top-1/2 -translate-y-1/2 flex justify-center p-10">
+						top-1/2 -translate-y-1/2 flex justify-center p-5 sm:p-10">
 							<h1 className="text-5xl text-gray-400 -tracking-wider font-sans font-bold">
 								{type === "tv" ? "SÉRIES" : "FILMES"}
 							</h1>
